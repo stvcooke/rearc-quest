e109755ad65c3419f473e7b30200f561292021f5
Steven Cooke <steve.w.cooke@gmail.com>
2021-04-28 10:29:51 -0400

    fixing path in certs

--- alb.tf
+++ alb.tf
@@ -125,8 +125,8 @@ data "aws_iam_policy_document" "s3_bucket_lb_write" {
 
 resource "aws_iam_server_certificate" "alb_cert" {
   name_prefix      = "rearc-quest"
-  certificate_body = file("keys/cert.pem")
-  private_key      = file("keys/key.pem")
+  certificate_body = file("${path.module}/keys/cert.pem")
+  private_key      = file("${path.module}/keys/key.pem")
 
   tags = var.tags
 }
